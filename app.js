d067f78a00f676ef08a880425be4ea6f163ca219
Kirill <kvn2001@internet.ru>
2024-09-04 01:50:58 +0300

    navbar

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav_item_text");
+
+hamburger.addEventListener("click", () => {
+  navItems.classList.toggle("active");
+  hamburger.classList.toggle("active");
+ 
+});
+
+
+
+    
+
+    
+    
